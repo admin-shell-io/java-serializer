c0ba7ce24378269afa4b9941c68374cfd4e5cca0
Jacoby <michael.jacoby@iosb.fraunhofer.de>
2021-06-02 16:57:31 +0200

    updated dynamic subclass resultion

--- src/main/java/io/adminshell/aas/v3/dataformat/json/AnnotationIntrospector.java
+++ src/main/java/io/adminshell/aas/v3/dataformat/json/AnnotationIntrospector.java
@@ -1,6 +1,5 @@
 package io.adminshell.aas.v3.dataformat.json;
 
-import com.fasterxml.jackson.annotation.JsonSubTypes;
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import com.fasterxml.jackson.databind.JavaType;
 import com.fasterxml.jackson.databind.cfg.MapperConfig;
@@ -10,20 +9,25 @@ import com.fasterxml.jackson.databind.introspect.JacksonAnnotationIntrospector;
 import com.fasterxml.jackson.databind.jsontype.NamedType;
 import com.fasterxml.jackson.databind.jsontype.TypeResolverBuilder;
 import io.adminshell.aas.v3.model.Referable;
-import io.github.classgraph.AnnotationInfo;
-import io.github.classgraph.AnnotationParameterValue;
 import io.github.classgraph.ClassGraph;
-import io.github.classgraph.ClassInfo;
 import io.github.classgraph.ClassInfoList;
 import io.github.classgraph.ScanResult;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class AnnotationIntrospector extends JacksonAnnotationIntrospector {
 
     private static final String MODEL_TYPE_PROPERTY = "modelType";
     private static final String TARGET_PACKAGE_NAME = "io.adminshell.aas.v3.model";
+    private ScanResult classInfo;
+
+    public AnnotationIntrospector() {
+        classInfo = new ClassGraph()
+                .enableClassInfo()
+                .acceptPackagesNonRecursive(TARGET_PACKAGE_NAME)
+                .scan();
+    }
 
     @Override
     public TypeResolverBuilder<?> findTypeResolver(MapperConfig<?> config, AnnotatedClass ac, JavaType baseType) {
@@ -49,13 +53,10 @@ public class AnnotationIntrospector extends JacksonAnnotationIntrospector {
 
     @Override
     public List<NamedType> findSubtypes(Annotated a) {
-        ScanResult scanResult = new ClassGraph()
-                .enableClassInfo()
-                .acceptPackagesNonRecursive(TARGET_PACKAGE_NAME)
-                .scan();
-//        if (a.getRawType().isInterface())
-        ClassInfoList subclasses = scanResult.getClassesImplementing(a.getName());
-        List<NamedType> result = subclasses.stream().map(x -> new NamedType(x.loadClass(), x.getSimpleName()))
+        ClassInfoList subclasses = classInfo.getClassesImplementing(a.getName());
+        List<NamedType> result = subclasses.stream()
+                .filter(x -> Stream.of(x.loadClass().getInterfaces()).anyMatch(y -> y.equals(a.getRawType())))
+                .map(x -> new NamedType(x.loadClass(), x.getSimpleName()))
                 .collect(Collectors.toList());
         return result;
 //        JsonSubTypes t = _findAnnotation(a, JsonSubTypes.class);
