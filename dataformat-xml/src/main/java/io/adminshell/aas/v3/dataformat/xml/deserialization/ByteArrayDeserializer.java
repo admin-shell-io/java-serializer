eeb9c5d70a77825dd2aa232266cf0ca91026464d
Sebastian Bader <sebastian.r.bader@web.de>
2021-07-01 18:59:38 +0200

    Adding copyright statements to all classes

--- dataformat-xml/src/main/java/io/adminshell/aas/v3/dataformat/xml/deserialization/ByteArrayDeserializer.java
+++ dataformat-xml/src/main/java/io/adminshell/aas/v3/dataformat/xml/deserialization/ByteArrayDeserializer.java
@@ -1,3 +1,29 @@
+/*
+Copyright (c) 2021 Fraunhofer IOSB-INA Lemgo,
+    eine rechtlich nicht selbstaendige Einrichtung der Fraunhofer-Gesellschaft
+    zur Foerderung der angewandten Forschung e.V.
+
+Copyright (c) 2021 Fraunhofer IOSB-ILT Karlsruhe,
+    eine rechtlich nicht selbstaendige Einrichtung der Fraunhofer-Gesellschaft
+    zur Foerderung der angewandten Forschung e.V.
+
+Copyright (c) 2021 Fraunhofer IAIS,
+    eine rechtlich nicht selbstaendige Einrichtung der Fraunhofer-Gesellschaft
+    zur Foerderung der angewandten Forschung e.V.
+
+Copyright (c) 2021 Fraunhofer IESE,
+    eine rechtlich nicht selbstaendige Einrichtung der Fraunhofer-Gesellschaft
+    zur Foerderung der angewandten Forschung e.V.
+
+Copyright (c) 2021 Fraunhofer IWU Karlsruhe,
+    eine rechtlich nicht selbstaendige Einrichtung der Fraunhofer-Gesellschaft
+    zur Foerderung der angewandten Forschung e.V.
+
+This source code is licensed under the Apache License 2.0 (see LICENSE.txt).
+
+This source code may use other Open Source software components (see LICENSE.txt).
+*/
+
 package io.adminshell.aas.v3.dataformat.xml.deserialization;
 
 import com.fasterxml.jackson.core.JsonParser;
@@ -12,13 +38,13 @@ import java.nio.charset.StandardCharsets;
 public class ByteArrayDeserializer extends JsonDeserializer<byte[]> {
 
     public static final Charset DEFAULT_CHARSET = StandardCharsets.UTF_8;
-    
+
     private final Charset charset;
-    
+
     public ByteArrayDeserializer() {
         this.charset = DEFAULT_CHARSET;
     }
-    
+
     public ByteArrayDeserializer(Charset charset) {
         this.charset = charset;
     }
--- dataformat-xml/src/main/java/io/adminshell/aas/v3/dataformat/xml/deserialization/DataSpecificationDeserializer.java
+++ dataformat-xml/src/main/java/io/adminshell/aas/v3/dataformat/xml/deserialization/DataSpecificationDeserializer.java
@@ -1,3 +1,29 @@
+/*
+Copyright (c) 2021 Fraunhofer IOSB-INA Lemgo,
+    eine rechtlich nicht selbstaendige Einrichtung der Fraunhofer-Gesellschaft
+    zur Foerderung der angewandten Forschung e.V.
+
+Copyright (c) 2021 Fraunhofer IOSB-ILT Karlsruhe,
+    eine rechtlich nicht selbstaendige Einrichtung der Fraunhofer-Gesellschaft
+    zur Foerderung der angewandten Forschung e.V.
+
+Copyright (c) 2021 Fraunhofer IAIS,
+    eine rechtlich nicht selbstaendige Einrichtung der Fraunhofer-Gesellschaft
+    zur Foerderung der angewandten Forschung e.V.
+
+Copyright (c) 2021 Fraunhofer IESE,
+    eine rechtlich nicht selbstaendige Einrichtung der Fraunhofer-Gesellschaft
+    zur Foerderung der angewandten Forschung e.V.
+
+Copyright (c) 2021 Fraunhofer IWU Karlsruhe,
+    eine rechtlich nicht selbstaendige Einrichtung der Fraunhofer-Gesellschaft
+    zur Foerderung der angewandten Forschung e.V.
+
+This source code is licensed under the Apache License 2.0 (see LICENSE.txt).
+
+This source code may use other Open Source software components (see LICENSE.txt).
+*/
+
 package io.adminshell.aas.v3.dataformat.xml.deserialization;
 
 import static io.adminshell.aas.v3.dataformat.xml.DataSpecificationManager.PROP_DATA_SPECIFICATION;
