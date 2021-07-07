b82ca7a93e454321faf635de190d908864fb5568
Jacoby <michael.jacoby@iosb.fraunhofer.de>
2021-07-07 14:01:07 +0200

    removed outdated license header

--- dataformat-xml/src/test/java/io/adminshell/aas/v3/dataformat/xml/CustomProperty.java
+++ dataformat-xml/src/test/java/io/adminshell/aas/v3/dataformat/xml/CustomProperty.java
@@ -13,32 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-/*
-Copyright (c) 2021 Fraunhofer IOSB-INA Lemgo,
-    eine rechtlich nicht selbstaendige Einrichtung der Fraunhofer-Gesellschaft
-    zur Foerderung der angewandten Forschung e.V.
-
-Copyright (c) 2021 Fraunhofer IOSB-ILT Karlsruhe,
-    eine rechtlich nicht selbstaendige Einrichtung der Fraunhofer-Gesellschaft
-    zur Foerderung der angewandten Forschung e.V.
-
-Copyright (c) 2021 Fraunhofer IAIS,
-    eine rechtlich nicht selbstaendige Einrichtung der Fraunhofer-Gesellschaft
-    zur Foerderung der angewandten Forschung e.V.
-
-Copyright (c) 2021 Fraunhofer IESE,
-    eine rechtlich nicht selbstaendige Einrichtung der Fraunhofer-Gesellschaft
-    zur Foerderung der angewandten Forschung e.V.
-
-Copyright (c) 2021 Fraunhofer IWU Karlsruhe,
-    eine rechtlich nicht selbstaendige Einrichtung der Fraunhofer-Gesellschaft
-    zur Foerderung der angewandten Forschung e.V.
-
-This source code is licensed under the Apache License 2.0 (see LICENSE.txt).
-
-This source code may use other Open Source software components (see LICENSE.txt).
-*/
-
 package io.adminshell.aas.v3.dataformat.xml;
 
 import java.util.List;
