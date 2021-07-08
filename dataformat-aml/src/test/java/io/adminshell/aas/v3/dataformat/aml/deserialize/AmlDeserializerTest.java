f534c3cf53448ac4483f156d188271c80a9d8c6a
Jacoby <michael.jacoby@iosb.fraunhofer.de>
2021-07-08 10:12:13 +0200

    add license headers to aml

--- dataformat-aml/src/test/java/io/adminshell/aas/v3/dataformat/aml/deserialize/AmlDeserializerTest.java
+++ dataformat-aml/src/test/java/io/adminshell/aas/v3/dataformat/aml/deserialize/AmlDeserializerTest.java
@@ -1,9 +1,22 @@
+/*
+ * Copyright (c) 2021 Fraunhofer-Gesellschaft zur Foerderung der angewandten Forschung e. V.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package io.adminshell.aas.v3.dataformat.aml.deserialize;
 
 import com.fasterxml.jackson.dataformat.xml.XmlMapper;
-import io.adminshell.aas.v3.dataformat.DeserializationException;
 import io.adminshell.aas.v3.dataformat.aml.AmlDeserializer;
-import org.junit.Test;
 
 import java.io.*;
 
@@ -12,13 +25,6 @@ public class AmlDeserializerTest {
     private final AmlDeserializer deserializer = new AmlDeserializer();
 
     private final XmlMapper xmlMapper = new XmlMapper();
-
-    @Test
-    public void testCoffeeBeanExample() throws IOException, DeserializationException {
-        final String file = "src/test/resources/amlfile/coffee-bean.xml";
-        String xml = inputStreamToString(new FileInputStream(file));
-        deserializer.read(xml);
-    }
 
     private String inputStreamToString(InputStream inputStream) throws IOException {
         StringBuilder stringBuilder = new StringBuilder();
