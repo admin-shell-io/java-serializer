4c95d58d18d9000813f915e324522eff627ecd50
Matthias Böckmann <matthias.boeckmann@iais.fraunhofer.de>
2021-09-06 11:13:17 +0200

    Fixing capitalization of enum values in RDF serialization

--- dataformat-rdf/src/main/java/io/adminshell/aas/v3/dataformat/rdf/JsonLdEnumSerializer.java
+++ dataformat-rdf/src/main/java/io/adminshell/aas/v3/dataformat/rdf/JsonLdEnumSerializer.java
@@ -28,13 +28,34 @@ public class JsonLdEnumSerializer extends JsonSerializer<Enum<?>> {
 
     @Override
     public void serialize(Enum value, JsonGenerator gen, SerializerProvider provider) throws IOException {
+        //Generated Enum classes of the admin shell have @IRI annotations, which need to be used to provide proper RDF
         if(value.getClass().isEnum() && value.getClass().getName().startsWith("io.adminshell.aas."))
         {
+            //Try to get annotation value to get the IRI used in the ontology
             if(value.getClass().getAnnotation(IRI.class) != null && value.getClass().getAnnotation(IRI.class).value().length > 0)
             {
                 gen.writeStartObject();
                 gen.writeStringField("@type", value.getClass().getAnnotation(IRI.class).value()[0]);
-                gen.writeStringField("@id", translate(value.getClass(), value.name()));
+
+                //Try to extract exact IRI of the enum value, if present
+                try {
+                    var annotation = value.getClass().getField(value.name()).getAnnotation(IRI.class);
+                    if(annotation != null && annotation.value().length > 0)
+                    {
+                        gen.writeStringField("@id", annotation.value()[0]);
+                    }
+                    else
+                    {
+                        //Didn't find an @IRI annotation - fall back to using class annotation + field name
+                        gen.writeStringField("@id", translate(value.getClass(), value.name()));
+                    }
+                }
+                //Should be impossible
+                catch (NoSuchFieldException e)
+                {
+                    //Didn't find an @IRI annotation - fall back to using class annotation + field name
+                    gen.writeStringField("@id", translate(value.getClass(), value.name()));
+                }
                 gen.writeEndObject();
             }
             else
--- dataformat-rdf/src/test/java/io/adminshell/aas/v3/dataformat/rdf/SerializerTest.java
+++ dataformat-rdf/src/test/java/io/adminshell/aas/v3/dataformat/rdf/SerializerTest.java
@@ -15,7 +15,6 @@
  */
 package io.adminshell.aas.v3.dataformat.rdf;
 
-import io.adminshell.aas.v3.dataformat.rdf.Serializer;
 import io.adminshell.aas.v3.model.*;
 import io.adminshell.aas.v3.model.impl.*;
 import org.apache.jena.riot.RDFLanguages;
@@ -67,7 +66,7 @@ public class SerializerTest {
                 .submodels(submodel)
                 .build();
         String output = new Serializer().serialize(aasEnv, RDFLanguages.JSONLD);
-        //System.out.println(output);
+        System.out.println(output);
         Assert.assertTrue(output.contains("@context"));
         Assert.assertTrue(output.contains("rdf:"));
 
